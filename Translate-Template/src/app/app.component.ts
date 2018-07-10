4de0b24d811ff5eae87d9fc163e92635f73b712b
Quoc Viet IT <vuongquocviet1996@gmail.com>
2018-07-10 16:37:45 +0700

    Install library

--- Translate-Template/src/app/app.component.ts
+++ Translate-Template/src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [
+    "../assets/css/logo.css"
+  ]
 })
 export class AppComponent {
-  title = 'app';
+  title = '';
 }
